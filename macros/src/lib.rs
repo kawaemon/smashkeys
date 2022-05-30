ec39f2e4ed5323e7dab909bd6bb018903fd9ebd8
kawaemon <me@kawaemon.dev>
2022-05-30 23:11:07 +0900

    macros: use Span::call_site()

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -5,36 +5,36 @@ use syn::{
     parse_macro_input, Ident, LitChar, LitStr, Result, Token,
 };
 
-fn ident_or_litstr(stream: ParseStream) -> Result<(String, Span)> {
+fn ident_or_litstr(stream: ParseStream) -> Result<String> {
     let lookahead = stream.lookahead1();
 
     if lookahead.peek(Ident) {
         let ident: Ident = stream.parse()?;
-        Ok((ident.to_string(), ident.span()))
+        Ok(ident.to_string())
     } else if lookahead.peek(LitStr) {
         let lit: LitStr = stream.parse()?;
-        Ok((lit.value(), lit.span()))
+        Ok(lit.value())
     } else {
         Err(lookahead.error())
     }
 }
 
-fn str_to_char_array(text: &str, span: Span) -> TokenStream {
-    let text = text.chars().map(|c| LitChar::new(c, span));
+fn str_to_char_array(text: &str) -> TokenStream {
+    let text = text.chars().map(|c| LitChar::new(c, Span::call_site()));
     quote! { [ #(#text),* ] }
 }
 
 #[proc_macro]
 pub fn as_char_array(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let (text, span) = parse_macro_input!(input with ident_or_litstr);
-    str_to_char_array(&text, span).into()
+    let text = parse_macro_input!(input with ident_or_litstr);
+    str_to_char_array(&text).into()
 }
 
 #[proc_macro]
 pub fn segments(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     struct Input {
-        origin: Vec<(String, Span)>,
-        hira: Vec<(String, Span)>,
+        origin: Vec<String>,
+        hira: Vec<String>,
     }
 
     impl Parse for Input {
@@ -72,8 +72,8 @@ pub fn segments(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
             let segments = self
                 .origin
                 .into_iter()
-                .map(|(t, s)| str_to_char_array(&t, s))
-                .zip(self.hira.into_iter().map(|(t, s)| str_to_char_array(&t, s)))
+                .map(|t| str_to_char_array(&t))
+                .zip(self.hira.into_iter().map(|t| str_to_char_array(&t)))
                 .map(
                     |(origin, hira)| quote! { Segment::new(#origin.as_slice(), #hira.as_slice()) },
                 );
